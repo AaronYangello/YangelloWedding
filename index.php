fbef61b9f84959204252a0d8c7d385dd47a3b704
aaronyangello <ayangello@idtus.com>
2018-07-25 15:10:38 -0400

    Attempting to correct meta tags

---
+++ index.php
@@ -0,0 +1,20 @@
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Aaron &amp; Kelsea</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Aaron and Kelsea's Wedding" />
+	<meta name="keywords" content="Aaron, Yangello, Kelsea, Salvatore, Wedding" />
+	<meta name="author" content="Aaron Yangello" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+</head>
