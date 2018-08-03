e1d184771d2ea1d6b3bcbb9970302e0c8c30324d
aaronyangello <ayangello@idtus.com>
2018-08-03 14:30:49 -0400

    Added correct version of JQUERY

--- gallery-pages/through-the-years.php
+++ gallery-pages/through-the-years.php
@@ -51,7 +51,7 @@
     </div>
 </div>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script src="../js/jquery.easing.1.3.js"></script>
     <script src="../js/jquery.animate-enhanced.min.js"></script>
     <script src="../js/jquery.superslides.js" type="text/javascript" charset="utf-8"></script>
